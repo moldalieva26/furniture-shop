1896a38abc478797dba41f3940ad55de55024f43
Meerim <moldalieva26@gmail.com>
2021-01-27 08:22:27 -0500

    changed java version

--- src/test/java/com/furniture/shop/webappv1/WebAppV1ApplicationTests.java
+++ src/test/java/com/furniture/shop/webappv1/WebAppV1ApplicationTests.java
@@ -3,7 +3,9 @@ package com.furniture.shop.webappv1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.furnitureshop.app.v1.WebAppV1Application;
+
+@SpringBootTest(classes=WebAppV1Application.class)
 class WebAppV1ApplicationTests {
 
 	@Test
