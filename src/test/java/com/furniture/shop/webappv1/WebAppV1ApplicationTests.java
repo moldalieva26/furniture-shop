dcf690281c8c70ef3c3ca6ae58fd588c3d50033b
Meerim <moldalieva26@gmail.com>
2021-01-28 18:48:11 -0500

    Restructure pom.xml, changed versions, deleted test classes

--- src/test/java/com/furniture/shop/webappv1/WebAppV1ApplicationTests.java
+++ src/test/java/com/furniture/shop/webappv1/WebAppV1ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.furniture.shop.webappv1;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.furnitureshop.app.v1.WebAppV1Application;
